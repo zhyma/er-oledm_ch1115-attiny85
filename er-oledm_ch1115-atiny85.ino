f40571940b74ba1e7eb07dd6af8183b5ee38e3a7
Zhaoyuan Ma <zhaoyuanma23@gmail.com>
2023-07-28 00:07:00 -0400

    both software and hardware spi is tested by oscilloscope

--- er-oledm_ch1115-atiny85.ino
+++ er-oledm_ch1115-atiny85.ino
@@ -28,36 +28,15 @@ Test OK : Arduino DUE,Arduino mega2560,Arduino UNO Board
 uint8_t oled_buf[WIDTH * HEIGHT / 8];
 
 void setup() {
-//  Serial.begin(9600);
-//  Serial.print("OLED Example\n");
-
-  /* display an image of bitmap matrix */
-
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
   er_oled_begin();
+  delay(3000);
+  command(0xa7);
+  delay(3000);
 }
 
 void loop() {
-//  shiftOut(1, 2,MSBFIRST, 0x0f);
-////digitalWrite(0, HIGH);
-////digitalWrite(1, HIGH);
-////digitalWrite(2, HIGH);
-////digitalWrite(3, HIGH);
-////digitalWrite(4, HIGH);
-//delay(10);
-////digitalWrite(0, LOW);
-////digitalWrite(1, LOW);
-////digitalWrite(2, LOW);
-////digitalWrite(3, LOW);
-////digitalWrite(4, LOW);
-//delay(10);
-
-//er_oled_begin();
 delay(100);
-
-//  er_oled_display(oled_buf);
+//  SPI.transfer(0x0f);
+//command(0x0F);
+  er_oled_display();
 }
--- er_oled.cpp
+++ er_oled.cpp
@@ -16,23 +16,28 @@ void command(uint8_t cmd){
 void SPIWrite(uint8_t *buffer, int bufferLength) {
     int i;
     for (i = 0; i < bufferLength; i++) {
-//        SPI.transfer(buffer[i]);
-        shiftOut(1, 2,MSBFIRST, buffer[i]);
+        #ifdef _software_SPI
+          shiftOut(OLED_SDA, OLED_SCL, MSBFIRST, buffer[i]);
+        #else
+          SPI.transfer(buffer[i]);
+        #endif
     }
 }
 
 void er_oled_begin()
 {
-
+    #ifdef _software_SPI
+    pinMode(OLED_SDA, OUTPUT);
+    pinMode(OLED_SCL, OUTPUT);
+    #endif
+    
     pinMode(OLED_RST, OUTPUT);
     pinMode(OLED_DC, OUTPUT);
     digitalWrite(OLED_DC, HIGH);
-//    pinMode(OLED_CS, OUTPUT);
-//    SPI.begin();
-    
-//    SPI.setClockDivider(SPI_CLOCK_DIV128);
-    
-//    digitalWrite(OLED_CS, LOW);
+    #ifndef _software_SPI
+      SPI.begin();
+    #endif
+
     digitalWrite(OLED_RST, HIGH);
     delay(10);
     digitalWrite(OLED_RST, LOW);
@@ -71,22 +76,22 @@ void er_oled_begin()
     delay(100);
 }
 
-void er_oled_display(uint8_t* pBuf)
-{    uint8_t page,i;   
-    for (page = 0; page < PAGES; page++) {         
-        command(0xB0 + page);/* set page address */     
-        command(0x00);   /* set low column address */      
-        command(0x10);  /* set high column address */      
-        //digitalWrite(OLED_DC, HIGH);
-        //SPIWrite(pBuf, WIDTH); /* write data  one page*/
-        //pBuf += WIDTH;        
-        for(i = 0; i< WIDTH; i++ ) {
-          digitalWrite(OLED_DC, HIGH);
-//          SPIWrite(pBuf, 1);// write data one
-          shiftOut(1, 2,MSBFIRST, 0xff);
-//          *pBuf++;
-        }        
-    }
+void er_oled_display()
+{   
+  uint8_t page,i;   
+  uint8_t buff = {0xff};
+  for (page = 0; page < PAGES; page++) {         
+      command(0xB0 + page);/* set page address */     
+      command(0x00);   /* set low column address */      
+      command(0x10);  /* set high column address */      
+      //digitalWrite(OLED_DC, HIGH);
+      //SPIWrite(pBuf, WIDTH); /* write data  one page*/
+      //pBuf += WIDTH;        
+      for(i = 0; i< WIDTH; i++ ) {
+        digitalWrite(OLED_DC, HIGH);
+        SPIWrite(buff, 1);
+      }        
+  }
 }
 
 void er_oled_fill(uint8_t* buffer)
@@ -116,96 +121,7 @@ void er_oled_pixel(int x, int y, char color, uint8_t* buffer)
         buffer[x+(y/8)*WIDTH] &= ~(1<<(y%8));
 }
 
-void er_oled_char1616(uint8_t x, uint8_t y, uint8_t chChar, uint8_t* buffer)
-{
-	uint8_t i, j;
-	uint8_t chTemp = 0, y0 = y, chMode = 0;
 
-	for (i = 0; i < 32; i++) {
-		chTemp = pgm_read_byte(&Font1612[chChar - 0x30][i]);
-		for (j = 0; j < 8; j++) {
-			chMode = chTemp & 0x80? 1 : 0; 
-			er_oled_pixel(x, y, chMode, buffer);
-			chTemp <<= 1;
-			y++;
-			if ((y - y0) == 16) {
-				y = y0;
-				x++;
-				break;
-			}
-		}
-	}
-}
-
-void er_oled_char(unsigned char x, unsigned char y, char acsii, char size, char mode, uint8_t* buffer)
-{
-    unsigned char i, j, y0=y;
-    char temp;
-    unsigned char ch = acsii - ' ';
-    for(i = 0;i<size;i++) {
-        if(size == 12)
-        {
-            if(mode)temp = pgm_read_byte(&Font1206[ch][i]);
-            else temp = ~pgm_read_byte(&Font1206[ch][i]);
-        }
-        else 
-        {            
-            if(mode)temp = pgm_read_byte(&Font1608[ch][i]);
-            else temp = ~pgm_read_byte(&Font1608[ch][i]);
-        }
-        for(j =0;j<8;j++)
-        {
-            if(temp & 0x80) er_oled_pixel(x, y, 1, buffer);
-            else er_oled_pixel(x, y, 0, buffer);
-            temp <<= 1;
-            y++;
-            if((y-y0) == size)
-            {
-                y = y0;
-                x++;
-                break;
-            }
-        }
-    }
-}
-
-void er_oled_string(uint8_t x, uint8_t y, const char *pString, uint8_t Size, uint8_t Mode, uint8_t* buffer)
-{
-    while (*pString != '\0') {       
-        if (x > (WIDTH - Size / 2)) {
-            x = 0;
-            y += Size;
-            if (y > (HEIGHT - Size)) {
-                y = x = 0;
-            }
-        }
-        
-        er_oled_char(x, y, *pString, Size, Mode, buffer);
-        x += Size / 2;
-        pString++;
-    }
-}
-
-void er_oled_char3216(uint8_t x, uint8_t y, uint8_t chChar, uint8_t* buffer)
-{
-    uint8_t i, j;
-    uint8_t chTemp = 0, y0 = y, chMode = 0;
-
-    for (i = 0; i < 64; i++) {
-        chTemp = pgm_read_byte(&Font3216[chChar - 0x30][i]);
-        for (j = 0; j < 8; j++) {
-            chMode = chTemp & 0x80? 1 : 0; 
-            er_oled_pixel(x, y, chMode, buffer);
-            chTemp <<= 1;
-            y++;
-            if ((y - y0) == 32) {
-                y = y0;
-                x++;
-                break;
-            }
-        }
-    }
-}
 
 void er_oled_bitmap(uint8_t x,uint8_t y,const uint8_t *pBmp, uint8_t chWidth, uint8_t chHeight, uint8_t* buffer)
 {
