e54ef7534c85b8c36dec1d3b7b9b8d0f4f7819f2
Zhaoyuan Ma <zhaoyuanma23@gmail.com>
2023-08-06 19:34:04 -0400

    clean up init a little bit

--- er_oled.cpp
+++ er_oled.cpp
@@ -7,22 +7,18 @@ EastRising Technology Co.,LTD
 
 #include <Arduino.h>
 
-void command(uint8_t cmd){
-    digitalWrite(OLED_DC, LOW);
-//    delay(10);
-    SPIWrite(&cmd, 1);
-    digitalWrite(OLED_DC, HIGH);
+void sendCommand(uint8_t cmd){
+  digitalWrite(OLED_DC, LOW);
+  SPIWrite(cmd);
+  digitalWrite(OLED_DC, HIGH);
 }
 
-void SPIWrite(uint8_t *buffer, int bufferLength) {
-    int i;
-    for (i = 0; i < bufferLength; i++) {
-        #ifdef _software_SPI
-          shiftOut(OLED_SDA, OLED_SCL, MSBFIRST, buffer[i]);
-        #else
-          SPI.transfer(buffer[i]);
-        #endif
-    }
+void SPIWrite(uint8_t data) {
+  #ifdef _software_SPI
+    shiftOut(OLED_SDA, OLED_SCL, MSBFIRST, data);
+  #else
+    SPI.transfer(data);
+  #endif
 }
 
 void er_oled_begin()
@@ -45,53 +41,31 @@ void er_oled_begin()
     delay(10);
     digitalWrite(OLED_RST, HIGH);
     delay(100);
-    command(0xAE); /*display off*/ 
-    command(0x00); /*set lower column address*/ 
-    command(0x10); /*set higher column address*/
-    command(0xB0); /*set page address*/ 
-    command(0x40); /*set display start lines*/ 
-    command(0x81); /*contract control*/ 
-//    command(0x80); /*4d*/ 
-    command(0x00); /*4d*/ 
-    command(0x82); /* iref resistor set and adjust ISEG*/ 
-    command(0x00); 
-    command(0xA0); /*set segment remap 0xA0*/ 
-    command(0xA2); /*set seg pads hardware configuration*/ 
-    command(0xA4); /*Disable Entire Display On (0xA4/0xA5)*/ 
-    command(0xA6); /*normal / reverse*/ 
-    command(0xA8); /*multiplex ratio*/ 
-    command(0x3F); /*duty = 1/64*/ 
-    command(0xC0); /*Com scan direction 0XC0*/ 
-    command(0xD3); /*set display offset*/ 
-    command(0x00); /* */ 
-    command(0xD5); /*set osc division*/ 
-    command(0xa0); 
-    command(0xD9); /*set pre-charge period*/ 
-    command(0x22); 
-    command(0xdb); /*set vcomh*/ 
-    command(0x40); 
-    command(0x31); /* Set pump 7.4v */ 
-    command(0xad); /*set charge pump enable*/ 
-    command(0x8b); /*Set DC-DC enable (0x8a=disable; 0x8b=enable) */ 
-    
-    command(0xAF);
+
+    for (uint8_t i = 0; i < OLED_INIT_LEN; i++){
+      sendCommand(pgm_read_byte(&OLED_INIT_CMD[i]));
+    }
+
     delay(100);
 }
 
 void er_oled_display()
 {   
   uint8_t page,i;   
-  uint8_t buff = {0x0f};
+  uint8_t buff = 0x01;
   for (page = 0; page < PAGES; page++) {         
-      command(0xB0 + page);/* set page address */     
-      command(0x00);   /* set low column address */      
-      command(0x10);  /* set high column address */      
+      sendCommand(0xB0 + page);/* set page address */     
+      sendCommand(0x00);   /* set low column address */      
+      sendCommand(0x10);  /* set high column address */      
       //digitalWrite(OLED_DC, HIGH);
       //SPIWrite(pBuf, WIDTH); /* write data  one page*/
       //pBuf += WIDTH;        
       for(i = 0; i< WIDTH; i++ ) {
         digitalWrite(OLED_DC, HIGH);
-        SPIWrite(buff, 1);
+        buff = buff << 1;
+        if (buff==0)
+          buff = 0x01;
+        SPIWrite(buff);
       }        
   }
 }
--- er_oled.h
+++ er_oled.h
@@ -22,12 +22,35 @@ EastRising Technology Co.,LTD
 #define OLED_RST 3
 #define OLED_DC  4
 
+#define OLED_INIT_LEN   28
+const uint8_t OLED_INIT_CMD[] PROGMEM = {
+  0xAE,             // display off
+  0x00, 0x10, 0xB0, // set lower column address, higher column address, and page address
+  0x40,             //set display start lines
+  0x81, 0x00,       //contract control, default 0x80
+  0x82, 0x00,       //iref resistor set and adjust ISEG
+  0xA0,             // set segment remap 0xA0
+  0xA2,             // set seg pads hardware configuration
+  0xA4,             // disable entire display on (0xA4, 0xA5)
+  0xA6,             // normal (0xA7 is reverse)
+  0xA8,             // multiplex ratio
+  0x3F,             // duty = 1/64
+  0xC0,             // Com scan direction 0xC0
+  0xD3, 0x00,       // set display offset
+  0xD5, 0xA0,       // set osc division
+  0xD9, 0x22,       // set pre-charge period
+  0xDB, 0x40,       // set vcomh
+  0x31,             // set pump 7.4v
+  0xAD,             // set charge pump enable
+  0x8B,             // set dc-dc enable (0x8A=disable; 0x8B=enable)
+  0xAF
+};
 
 void er_oled_begin();
 //void er_oled_display(uint8_t* buffer);
 void er_oled_display();
-void SPIWrite(uint8_t *buffer, int bufferLength);
-void command(uint8_t cmd);
+void SPIWrite(uint8_t data);
+void sendCommand(uint8_t cmd);
 
 
 #endif
